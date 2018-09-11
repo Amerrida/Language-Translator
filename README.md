2fd8f8e3fc80e7ec5a1dc9f1a57dbc54432fbabd
Your NameAftan <aftan.merrida@gmail.com>
2018-09-11 17:59:49 -0500

    done

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # Language -Translator
+
+### Holiday Language Assignment
+This is a Holiday Translator
+
+
+## How to run this project
+
+
+* Use npm to instal http-server : 
+``` 
+npm install -g http-server
+
+````
+* Run the server
+
+``` sh
+hs  -p 9999
+```
+* Open Chrome and navaigate to: 
+```
+localhost: 8081
+```
+``` js
+console.log('hi')
+
--- main.js
+++ main.js
@@ -1,1 +1,56 @@
-console.log("hi")
+//List of words//
+
+const german = {
+    "merry": "frohe", 
+    "christmas":"weihnachten", 
+    "and":"und", 
+    "happy":"glückliches", 
+    "new":"neues", 
+    "year":"jahr"
+};
+
+const spanish = {
+    "merry": "feliz", 
+    "christmas":"navidad", 
+    "and":"y", 
+    "happy":"próspero", 
+    "new":"nuevo", 
+    "year":"año"
+};
+
+const french = {
+    "merry": "joyeux", 
+    "christmas":"noel", 
+    "and":"et", 
+    "happy":"bonne", 
+    "new":"nouveau", 
+    "year":"année"
+};
+
+
+
+const printToDom = (stringToPrint, divId) => {
+    const selectedDiv = document.getElementById(divId);
+    selectedDiv.innerHTML = stringToPrint;
+};
+
+let outputString = '';
+const translatorLoop = (language) => {
+outputString = '';
+let inputText = document.getElementById('textarea').value.toLowerCase();
+let inputArray = inputText.split(' ');
+for (i = 0; i < inputArray.length; i++) {
+    outputString += `${language[inputArray[i]]}`;
+    outputString += " ";
+};
+printToDom(outputString,'emptyDiv');
+};
+//Functions//
+let germanButtonElement = document.getElementById('germanButton');
+germanButtonElement.addEventListener('click', function(){translatorLoop(german)});
+
+let spanishButtonElement = document.getElementById('spanishButton');
+spanishButtonElement.addEventListener('click', function(){translatorLoop(spanish)});
+
+let frenchButtonElement = document.getElementById('frenchButton');
+frenchButtonElement.addEventListener('click', function(){translatorLoop(french)});
